2afac4185c0cafcf2cf757bc681549fce259d8f4
Junaid <junaidkabani3@gmail.com>
2024-10-03 05:11:47 +0100

    Fix getFilteredMatches code

--- keystone/extendGraphqlSchema/queries/getFilteredMatches.js
+++ keystone/extendGraphqlSchema/queries/getFilteredMatches.js
@@ -1,34 +1,26 @@
 async function getFilteredMatches(root, args, context) {
-  // Fetch all matches with input and output quantity equals to 1
+  // Fetch all matches
   const matches = await context.query.Match.findMany({
-    where: {
-      input: {
-        every: {
-          quantity: { equals: 1 },
-        },
-      },
-      output: {
-        every: {
-          quantity: { equals: 1 },
-        },
-      },
-    },
-    query: `id input { id quantity productId variantId lineItemId externalDetails { title image price productLink inventory inventoryTracked } shop { id name } } output { id quantity productId variantId lineItemId externalDetails { title image price productLink inventory inventoryTracked } price channel { id name } }`,
+    query: `
+      id 
+      outputPriceChanged
+      inventoryNeedsToBeSynced { syncEligible sourceQuantity targetQuantity }
+      input { 
+        id quantity productId variantId lineItemId 
+        externalDetails { title image price productLink inventory inventoryTracked } 
+        shop { id name } 
+      } 
+      output { 
+        id quantity productId variantId lineItemId 
+        externalDetails { title image price productLink inventory inventoryTracked } 
+        price channel { id name } 
+      }
+    `,
   });
 
-  // Filter matches to include only those with exactly one input and one output
-  const filteredMatches = matches.filter((match) => {
-    return (
-      match.input.length === 1 &&
-      match.output.length === 1 &&
-      match.input[0].quantity === 1 &&
-      match.output[0].quantity === 1 &&
-      match.input[0].externalDetails.inventory &&
-      match.output[0].externalDetails.inventory &&
-      match.input[0].externalDetails.inventory !==
-        match.output[0].externalDetails.inventory
-    );
-  });
+  // console.log(matches);
+  // Filter matches based on inventoryNeedsToBeSynced.syncEligible
+  const filteredMatches = matches.filter(match => match.inventoryNeedsToBeSynced.syncEligible);
 
   // Return the filtered matches
   return filteredMatches;
